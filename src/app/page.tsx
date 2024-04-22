8b843236a843b6f7cccdb5b7c3aff56184c6d143
James-Lee-01 <freebike01@gmail.com>
2024-04-23 00:46:38 +0800

    style: adjust line-height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <main className="flex flex-col">
       <section className="title-section flex flex-col justify-center items-center mt-24 gap-1.5">
         <div className="main-title">
-          <p className="text-[46px] font-gothamBold">Best Seller Gaming PC</p>
+          <p className="text-[46px] font-gothamBold leading-[51px]">Best Seller Gaming PC</p>
         </div>
         <div className="second-title">
-          <p className="text-[30px] font-gothamBold">Prebuilt & Custom</p>
+          <p className="text-[30px] font-gothamBold leading-[33px]">Prebuilt & Custom</p>
         </div>
       </section>
       <section className="card-section"></section>
